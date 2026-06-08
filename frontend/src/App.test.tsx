58f68bdaf02234317bacacaa2c5a458f3d6e626a
JR_Pro <jordan.renoult33@gmail.com>
2026-06-08 09:14:34 +0200

    ajout commentaires app.test.tsx bloque la creation de compte si mots de passe différents

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -317,7 +317,7 @@ describe("App", () => {
     expect(await screen.findByText("Identifiants invalides.")).toBeInTheDocument();
   });
 
-  it("bloque la creation de compte si les mots de passe different", async () => {
+  it("bloque la creation de compte si mots de passe differents", async () => {
     // Testing Library : Création d'un utilisateur virtuel pour simuler les interactions.
     const viewer = userEvent.setup();
 
