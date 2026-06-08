d0c8f09043098dbc1879298a1661fd1edb6a7daa
JR_Pro <jordan.renoult33@gmail.com>
2026-06-08 10:49:41 +0200

    ajout commentaire user entity ts pour typeorm

--- backend/src/users/user.entity.ts
+++ backend/src/users/user.entity.ts
@@ -5,7 +5,8 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn
 } from "typeorm";
-
+// Type ORM va lire les décorateurs pour créer la table correspondante dans la base de données 
+// et gérer les opérations CRUD (create, read, update, delete) sur les entités User.
 @Entity({ name: "users" })
 export class User {
   @PrimaryGeneratedColumn("uuid")
